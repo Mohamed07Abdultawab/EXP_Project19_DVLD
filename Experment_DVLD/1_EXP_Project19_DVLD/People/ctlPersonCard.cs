9f671222ce7f9a3a58f7fd8a145bbff525a088d3
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2025-11-26 21:37:42 +0200

    create control (perosn card) and his code

--- Experment_DVLD/1_EXP_Project19_DVLD/People/ctlPersonCard.cs
+++ Experment_DVLD/1_EXP_Project19_DVLD/People/ctlPersonCard.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using _2_BusinessLayer;
+using EXP_Project19_DVLD.Properties;
 
 namespace EXP_Project19_DVLD.People
 {
@@ -17,6 +20,103 @@ namespace EXP_Project19_DVLD.People
             InitializeComponent();
         }
 
-        
+        clsPerson _Person;
+        private int _PersonID = -1;
+        public int PersonID
+        {
+            get { return _PersonID; }
+            set
+            {
+                _PersonID = value;
+            }
+        }
+
+        public clsPerson SelectedPersonInfo
+        {
+            get { return _Person; }
+        }
+
+        public void LoadPersonInfo(int personID)
+        {
+            _Person = clsPerson.Find(personID);
+            if (_Person != null)
+            {
+                _FillPersonInfo();
+            }
+            else
+            {
+                MessageBox.Show("Person not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ResetPersonInfo();
+            }
+        }
+
+        public void LoadPersonInfo(string NationalNo)
+        {
+            _Person = clsPerson.Find(NationalNo);
+            if (_Person != null)
+            {
+                _FillPersonInfo();
+            }
+            else
+            {
+                MessageBox.Show("Person not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ResetPersonInfo();
+            }
+        }
+
+        public void _LoadPersonImage()
+        {
+            if (_Person.Gendor == 0)
+                pbPersonImage.Image = Resources.Male_512;
+            else
+                pbPersonImage.Image = Resources.Female_512;
+
+            string ImagePath = _Person.ImagePath;
+            if (ImagePath != null)
+                if (File.Exists(ImagePath))
+                    pbPersonImage.ImageLocation = ImagePath;
+                else
+                    MessageBox.Show("Could not find this image: = " + ImagePath, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void _FillPersonInfo()
+        {
+            llEditPersonInfo.Enabled = true;
+            _PersonID = _Person.PersonID;
+            lblPersonID.Text = _Person.PersonID.ToString();
+            lblNationalNo.Text = _Person.NationalNo;
+            lblFullName.Text = _Person.FullName;
+            lblGendor.Text = _Person.Gendor == 0 ? "Male" : "Female";
+            lblEmail.Text = _Person.Email;
+            lblPhone.Text = _Person.Phone;
+            lblDateOfBirth.Text = _Person.BirthDate.ToShortDateString();
+            lblCountry.Text = clsCountry.Find(_Person.NationalityCountryID).CountryName;
+            lblAddress.Text = _Person.Address;
+            _LoadPersonImage();
+
+        }
+
+        public void ResetPersonInfo()
+        {
+            lblPersonID.Text = "[????]";
+            lblNationalNo.Text = "[????]";
+            lblFullName.Text = "[????]";
+            pbGendor.Image = Resources.Man_32;
+            lblGendor.Text = "[????]";
+            lblEmail.Text = "[????]";
+            lblPhone.Text = "[????]";
+            lblDateOfBirth.Text = "[????]";
+            lblCountry.Text = "[????]";
+            lblAddress.Text = "[????]";
+            pbPersonImage.Image = Resources.Male_512;
+
+        }
+
+        private void llEditPersonInfo_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmAddUpdatePersone frm = new frmAddUpdatePersone(_PersonID);
+            frm.ShowDialog();
+            LoadPersonInfo(_PersonID);
+        }
     }
 }
